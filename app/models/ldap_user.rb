d6f465095a0bb1d20eb38b444a652f78ac3b7a25
Emil Edholm <bobby@verypublic.org>
2015-01-06 19:32:40 +0100

    Parse pushService

--- app/models/ldap_user.rb
+++ app/models/ldap_user.rb
@@ -7,6 +7,13 @@ class LdapUser < ActiveLdap::Base
     @full_name ||= "#{gn} '#{nickname}' #{sn}"
   end
 
+  def push_services
+     @push_services ||= self.pushService.map do |s|
+       service, device, key = s.split ";"
+       {service: service, device: device, api: key}
+     end
+  end
+
   def db_user
     @db_user ||= User.find_or_create_by(cid: uid)
   end
