e07c649484f7f4287bf71282008953a2a7694a48
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-01-07 23:12:57 +0100

    create user class for lookup in chalmers ldap

---
+++ app/models/chalmers_user.rb
@@ -0,0 +1,11 @@
+class ChalmersUser < ActiveLdap::Base
+  setup_connection host: 'ldap.chalmers.se', base: 'dc=chalmers,dc=se'
+  ldap_mapping dn_attribute: 'uid',
+               prefix: 'ou=people'
+
+  GROUP_BASE = 'cn=pr_ch_tkite,ou=groups,dc=chalmers,dc=se'
+
+  def it?
+    @it ||= search(filter: "member=#{dn}", scope: :base, attributes: ['memberOf'], base: GROUP_BASE).any?
+  end
+end
