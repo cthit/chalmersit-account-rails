59667e349bac578534208fd062fa1b375e9346f7
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-01-10 05:38:02 +0100

    restructure notifycations api

---
+++ lib/notifications/notify.rb
@@ -0,0 +1,7 @@
+module ChalmersIT
+  class Notify
+    def self.notify(users, title, message)
+
+    end
+  end
+end
--- lib/notifications/pushover.rb
+++ lib/notifications/pushover.rb
@@ -1,21 +1,32 @@
+require 'notifications/notify'
 require 'net/https'
 
-url = URI.parse("https://api.pushover.net/1/messages.json")
-req = Net::HTTP::Post.new(url.path)
-users = []
+module ChalmersIT
+  class Pushover < Notify
 
-res = Net::HTTP.new(url.host, url.port)
-res.use_ssl = true
-res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    @@url = URI.parse("https://api.pushover.net/1/messages.json")
+    @@req = Net::HTTP::Post.new(@@url.path)
 
-res.start do |http|
-  users.each do |u|
-    req.set_form_data({
-      :token => Rails.applications.secrets.pushover,
-      :user => u,
-      :message => "hello world",
-    })
-    response = http.request(req)
-    p response.body
+    def self.notify(users, title, message)
+
+      res = Net::HTTP.new(@@url.host, @@url.port)
+      res.use_ssl = true
+      res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      # open a single connection and send all the messages over that connection
+      res.start do |http|
+        users.each do |user|
+          @@req.set_form_data({
+            token: Rails.application.secrets.pushover,
+            user: user,
+            message: message,
+            title: title
+          })
+          response = http.request(@@req)
+          p response.body
+        end
+      end
+    end
+
   end
 end
