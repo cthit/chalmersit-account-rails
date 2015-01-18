423c65f507c82b160477e22a1f971289b91d8298
Emil Edholm <bobby@verypublic.org>
2015-01-18 06:03:26 +0100

    List group members

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
   def on_pages? *pages
     pages.any?{|p| current_page? p}
   end
+
+  def is_me? user
+    current_user && current_user == user.db_user
+  end
+
+  def user_or_me user
+    if is_me? user
+      me_path
+    else
+      user_path(user.uid)
+    end
+  end
 end
