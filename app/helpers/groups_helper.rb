f41b4cdc4908c5fb863d2c6d6a8a30cd70697024
Emil Edholm <bobby@verypublic.org>
2015-01-20 03:34:48 +0100

    Prettier group page

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,9 +1,20 @@
 module GroupsHelper
-  def print_if_present tag, str
-    if str.nil?
-      return
-    else
-      content_tag(tag, str)
+  def dd_link icon, text, link
+    content_tag :dt, fa_icon(icon, text: text) + (content_tag :dd, (link_to link))
+  end
+
+  def dd_homepages homepages
+    homepages.each do |www|
+      if www.include? 'facebook.com'
+        concat dd_link 'facebook', 'facebook', www
+      elsif www.include? 'twitter.com'
+        concat dd_link 'twitter', 'Twitter', www
+      elsif www.include? 'github.com'
+        concat dd_link 'github', 'GitHub', www
+      else
+        concat dd_link 'external-link', t('homepage', count: 1), www
+      end
     end
+    nil
   end
 end
