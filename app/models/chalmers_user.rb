f154ebdfcb884da22c0a2f96aa9bfc9e58d5d229
Emil Edholm <bobby@verypublic.org>
2015-02-03 23:38:30 +0100

    Check if already registered

--- app/models/chalmers_user.rb
+++ app/models/chalmers_user.rb
@@ -4,6 +4,15 @@ class ChalmersUser
 
   validates :cid, presence: true, length: {within: 3..10}, format: {with: /\A[a-z]+[0-9a-z]*\z/, }
   validate :valid_password?
+  validate :havent_already_registered?
+
+  def havent_already_registered?
+    if cid.present?
+      if LdapUser.search(attribute: :uid, value: cid, limit:1).any?
+        errors.add(:cid, :already_registered)
+      end
+    end
+  end
 
   def valid_password?
     unless password.present?
@@ -15,13 +24,13 @@ class ChalmersUser
     end
 
     if errors.empty?
-      #begin
-      #  krb5 = Kerberos::Krb5.new
-      #  # Authenticate against Kerberos. raises Kerberos::Krb5::Exception on wrong password.
-      #  krb5.get_init_creds_password cid, password
-      #rescue Kerberos::Krb5::Exception
-      #  errors.add(:base, :wrong_cid_or_pass)
-      #end
+      begin
+        krb5 = Kerberos::Krb5.new
+        # Authenticate against Kerberos. raises Kerberos::Krb5::Exception on wrong password.
+        krb5.get_init_creds_password cid, password
+      rescue Kerberos::Krb5::Exception
+        errors.add(:base, :wrong_cid_or_pass)
+      end
     end
   end
 
