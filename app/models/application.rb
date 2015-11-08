5ae5b953552363d2bb1e41a6aacf3d00742657e9
oskar367 <vilddjur@localhost.localdomain>
2015-11-08 21:45:53 +0100

    added comments, method for getting supported push services

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,9 @@
+# Merely a representation of the application, used when displaying all the subscribable applications
 class Application < ActiveRecord::Base
   has_many :subscribable_types
+
+  # Called to get a array of supported subscribable types
+  def supported_subs
+    ["pushover"]
+  end
 end
