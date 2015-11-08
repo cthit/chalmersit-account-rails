7a2c20bc91bc41dcb2dbbde819d280178a2c8aab
oskar367 <vilddjur@localhost.localdomain>
2015-11-08 21:51:01 +0100

    added method to send tokens to a service

--- app/models/service_data.rb
+++ app/models/service_data.rb
@@ -1,5 +1,23 @@
 class ServiceData < ActiveRecord::Base
   belongs_to :user
   belongs_to :subscription
-  belongs_to :notification_service
+
+  def send(tokens)
+    if push_client == :pushover
+      users = []
+      devices.split(";").each do |device|
+        users.push({:user => get_service_key("pushover"), device: device})
+      end
+      Chalmersit::Pushover.notify(users, tokens)
+    else
+      puts "not yet implemented"
+    end
+  end
+
+  private
+  def get_service_key(service)
+    push_services = LdapUser.find(user.cid).pushService.split(";")
+    index = push_services.find_index(service)# gets the index where the service
+    push_services[index + 1] # selects the key which belongs to the service
+  end
 end
