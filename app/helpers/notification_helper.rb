4e3bc5ac4aa0bb0c1b96226d796490b356f396cf
oskar367 <vilddjur@localhost.localdomain>
2015-11-10 16:57:38 +0100

    added comments

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,5 +1,6 @@
 module NotificationHelper
-
+  # Notifies all subscribers of a given application, with application_id
+  # Expects 'tokens' of format; [{:message: "Message", title: "Title", :url_title: "http://google.se"}]
   def notify_subscibers(app_id, tokens)
     subscriptions = Subscription.where(:application_id => app_id)
     subscriptions.each do |sub|
