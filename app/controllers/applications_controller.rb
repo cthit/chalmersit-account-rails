7489a339f97d53a4765ca529d87d3713e689e5d5
oskar367 <oskar.jedvert@gmail.com>
2015-11-12 09:43:02 +0100

    added basic view and controller for applications from user perspective

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationsController < ApplicationController
+  before_action :set_application, only: [:show]
+
+  def index
+    @applications = Application.all
+    render 'applications/index'
+  end
+  def show
+    render 'applications/show'
+  end
+private
+  def set_application
+    @application = Application.find(params[:id])
+  end
 end
